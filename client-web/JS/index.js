97241087858a4222ec521abf27daf1839819f34b
Magliaa <magliani.andrea03@gmail.com>
2024-06-29 15:47:44 +0200

    :sparkles: Add renew domain

--- client-web/JS/index.js
+++ client-web/JS/index.js
@@ -1,40 +1,37 @@
 const API_URI = "http://localhost:8080";
 
+// get a "domains/{{domainName}}, ritorna le informazioni del dominio
 const getDomainDetail = async (domainName) => {
   const response = await fetch(API_URI + "/domains/" + domainName);
   const data = await response.json();
   return data;
 };
 
+// get a "domains/user/{userId}, ritorna i domini registrati dall'utente
 const getUserDomains = async (uid) => {
   const response = await fetch(API_URI + "/domains/user/" + uid);
   const data = await response.json();
   return data;
 };
 
-const getUserDetail = async (uid) => {
-  const response = await fetch(API_URI + "/users/" + uid);
-  const data = await response.json();
-  return data;
-};
-
+// get a /orders/{userId}, ritorna gli ordini dell'utente (anche quelli scaduti)
 const getOrders = async (uid) => {
   const response = await fetch(API_URI + "/orders/" + uid);
   const data = await response.json();
   return data;
 };
 
+// post a /domains/register/, permette di registrare un dominio
 const buyDomain = async (
   uid,
   domainName,
   registerTime,
   cardNumber,
-  expDate,
   cvv,
   cardOwnerName,
   cardOwnerSurname
 ) => {
-  const response = await fetch(API_URI + "/buy-domain/", {
+  const response = await fetch(API_URI + "/domains/register/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -42,8 +39,8 @@ const buyDomain = async (
     body: JSON.stringify({
       uid,
       domainName,
+      registerTime,
       cardNumber,
-      expDate,
       cvv,
       cardOwnerName,
       cardOwnerSurname,
@@ -53,6 +50,7 @@ const buyDomain = async (
   return data;
 };
 
+// post a /user/signup, permette di registrare un utente
 const userSignUp = async (name, surname, email) => {
   const response = await fetch(API_URI + "/user/signup", {
     method: "POST",
@@ -69,27 +67,32 @@ const userSignUp = async (name, surname, email) => {
   return data;
 };
 
-const renewDomain = async (id, renewPeriod) => {
-  const response = await fetch(API_URI + "/renew-domain/", {
+// post a /renew-domain permette di rinnovare un dominio
+const renewDomain = async (userId, domainName, renewTime) => {
+  const response = await fetch(API_URI + "/domains/renew", {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      id,
-      renewPeriod,
+      userId,
+      domainName,
+      renewTime,
     }),
   });
   const data = await response.json();
   return data;
 };
 
-const createRegisterDomainCard = (domain) => {
+// funzione per costruire dinamicamente una card di dominio registrato
+const createRegisterDomainCard = (name, domain) => {
   const domainCard = document.createElement("div");
   domainCard.id = "domainCard";
   const domainCardText = document.createElement("p");
   domainCardText.id = "domainCardText";
-  domainCardText.innerText = `Dominio: ${domain.id} - Data di registrazione: ${domain.registrationDate} - Data di scadenza: ${domain.expiration} - Stato: ${domain.status}`;
+  domainCardText.innerText = `Dominio: ${name} - Data di registrazione: ${domain.registerDate} - Data di scadenza: ${domain.expirationDate}`;
+  domainCard.append(domainCardText)
+  return domainCard
 };
 
 window.addEventListener("load", () => {
@@ -107,6 +110,7 @@ window.addEventListener("load", () => {
   const registerDomainButton = document.getElementById("registerDomainButton");
   const renewDomainButton = document.getElementById("renewDomainButton");
 
+  // gestione della registrazione tramite localStorage, dopo la registrazione l'utente mantiene l'accesso fino al logout
   if (localStorage.getItem("userUid")) {
     domain.style.display = "block";
     userDomains.style.display = "none";
@@ -126,11 +130,13 @@ window.addEventListener("load", () => {
     document.getElementById("navSignUp").setAttribute("selected", "");
   }
 
+  // gestione del logout
   logout.addEventListener("click", () => {
     localStorage.removeItem("userUid");
     window.location.reload();
   });
 
+  // gestione della navbar per simulare una SPA (SinglePageApplication)
   navDomain.addEventListener("click", () => {
     document.querySelectorAll(".navItem[selected]").forEach((item) => {
       item.removeAttribute("selected");
@@ -144,6 +150,7 @@ window.addEventListener("load", () => {
     signUp.style.display = "none";
   });
 
+  // gestione della navbar per simulare una SPA (SinglePageApplication)
   navUserDomain.addEventListener("click", () => {
     document.querySelectorAll(".navItem[selected]").forEach((item) => {
       item.removeAttribute("selected");
@@ -159,13 +166,16 @@ window.addEventListener("load", () => {
     const userDomainsList = document.getElementById("userDomainsList");
 
     getUserDomains(localStorage.getItem("userUid")).then((domains) => {
-      domains.forEach((domain) => {
-        const domainCard = createRegisterDomainCard(domain);
+      userDomainsList.innerHTML = ''
+      const keys = Object.keys(domains)
+      keys.forEach((key) => {
+        const domainCard = createRegisterDomainCard(key, domains[key]);
         userDomainsList.appendChild(domainCard);
       });
     });
   });
 
+  // gestione della navbar per simulare una SPA (SinglePageApplication)
   navOrder.addEventListener("click", () => {
     document.querySelectorAll(".navItem[selected]").forEach((item) => {
       item.removeAttribute("selected");
@@ -179,19 +189,27 @@ window.addEventListener("load", () => {
     signUp.style.display = "none";
 
     getOrders(localStorage.getItem("userUid")).then((orders) => {
-      const orderList = document.getElementById("orderList");
-      orders.forEach((order) => {
+      const orderList = document.getElementById("ordersList");
+      orderList.innerHTML = ''
+      const keys = Object.keys(orders)
+      keys.forEach((key) => {
+        const order = orders[key]
         const orderCard = document.createElement("div");
         orderCard.id = "orderCard";
         const orderCardText = document.createElement("p");
         orderCardText.id = "orderCardText";
-        orderCardText.innerText = `Dominio: ${order.domainName} - Data: ${order.date} - Tipo: ${order.type} - Prezzo: ${order.price}`;
+        orderCardText.innerText = `Dominio: ${order.domain} - Data di scadenza: ${order.cardExpireDate} - Tipo: ${order.type} - Prezzo: ${order.price}`;
         orderCard.appendChild(orderCardText);
         orderList.appendChild(orderCard);
       });
+
+      if (keys.length === 0) {
+        orderList.innerHTML = 'Nessun ordine effettuato!'
+      }
     });
   });
 
+  // gestione della navbar per simulare una SPA (SinglePageApplication)
   navSignUp.addEventListener("click", () => {
     document.querySelectorAll(".navItem[selected]").forEach((item) => {
       item.removeAttribute("selected");
@@ -205,6 +223,7 @@ window.addEventListener("load", () => {
     signUp.style.display = "block";
   });
 
+  // gestione dell'evento di registrazione, (prende tutti i dati dal form e li invia tramite api)
   signUpButton.addEventListener("click", () => {
     const name = document.getElementById("name").value;
     const surname = document.getElementById("surname").value;
@@ -231,6 +250,7 @@ window.addEventListener("load", () => {
       });
   });
 
+  // gestione dell'evento di ricerca per nome dominio
   search.addEventListener("click", () => {
     const domainName = document.getElementById("domainName").value;
     const domainInfo = document.getElementById("domainInfo");
@@ -241,33 +261,34 @@ window.addEventListener("load", () => {
     }
 
     getDomainDetail(domainName)
-      .then((domain) => {
-        if (domain.ownership) {
-          domainInfo.innerText = `Il dominio cercato appartiene gia ad un altro utente: Proprietario: ${domain.ownership.name} ${domain.ownership.surname} - Email: ${domain.ownership.email} - Data di scadenza: ${domain.expiration}`;
+      .then((risp) => {
+        const domain = risp.domain
+        const user = risp.user
+        domainInfo.innerHTML = ''
+        registerForm.style.display = 'none'
+        if (domain.ownershipUserId) {
+          domainInfo.innerText = `\n Il dominio cercato appartiene fino al ${domain.expirationDate} all'utente: ${user.name} ${user.surname} - email: ${user.email}`;
         } else {
-          domainInfo.innerText = `Il dominio cercato è disponibile: Prezzo: ${domain.price}`;
           registerForm.style.display = "block";
         }
-      })
-      .catch(() => {
-        domainInfo.innerText = "Dominio non trovato";
-      });
+      }).catch(() => {
+        domainInfo.innerHTML = ''
+        registerForm.style.display = "block";
+    })
   });
 
+  // gestione del pulsante di registra dominio, (prende tutti i dati dal form e li invia tramite api)
   registerDomainButton.addEventListener("click", () => {
-    const expirationDate = document.getElementById("expirationDate").value;
+    const registerTime = document.getElementById("registerTime").value;
     const domainName = document.getElementById("domainName").value;
     const cardNumber = document.getElementById("cardNumber").value;
-    const expDate = document.getElementById("expDate").value;
     const cvv = document.getElementById("cvv").value;
     const cardFirstName = document.getElementById("cardFirstName").value;
     const cardLastName = document.getElementById("cardLastName").value;
 
     if (
-      expirationDate === "" ||
       domainName === "" ||
       cardNumber === "" ||
-      expDate === "" ||
       cvv === "" ||
       cardFirstName === "" ||
       cardLastName === ""
@@ -276,20 +297,56 @@ window.addEventListener("load", () => {
       return;
     }
 
+    if (registerTime > 10 || registerTime < 0) {
+      alert("Il tempo di registrazione può essere al massimo 10 anni e al minimo 0")
+      return
+    }
+
     buyDomain(
       localStorage.getItem("userUid"),
       domainName,
+      registerTime,
       cardNumber,
-      expDate,
       cvv,
       cardFirstName,
       cardLastName
     )
-      .then(() => {
-        alert("Dominio acquistato con successo");
-      })
-      .catch(() => {
-        alert("Errore durante l'acquisto");
-      });
+    .then(() => {
+      alert("Dominio acquistato con successo");
+    })
+    .catch(() => {
+      alert("Errore durante l'acquisto");
+    });
   });
+
+  renewDomainButton.addEventListener("click", () => {
+    const domainName = document.getElementById("renewDomainName").value
+    const renewTime = document.getElementById("renewDomainDate").value
+
+    if (domainName === '' || renewTime === '' || renewTime > 10 || renewTime < 0) {
+      alert("Compila i campi correttamente!")
+      return
+    }
+
+    renewDomain(
+        localStorage.getItem("userUid"),
+        domainName,
+        renewTime
+    ).then(() => {
+      alert("Rinnovo avvenuto correttamente")
+      const userDomainsList = document.getElementById("userDomainsList");
+
+      getUserDomains(localStorage.getItem("userUid")).then((domains) => {
+        userDomainsList.innerHTML = ''
+        const keys = Object.keys(domains)
+        keys.forEach((key) => {
+          const domainCard = createRegisterDomainCard(key, domains[key]);
+          userDomainsList.appendChild(domainCard);
+        });
+      });
+    }).catch(() => {
+      alert("Errore durante il rinnovo del dominio!")
+    })
+  })
+
 });
