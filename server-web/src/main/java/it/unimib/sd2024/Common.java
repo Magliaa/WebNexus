771da25e07ad70c7c99f6fd375c1f49a8db0a3e3
Theofilia Jessica <115350137+KatsuuCurry@users.noreply.github.com>
2024-06-30 19:07:51 +0200

    :bug: Fixed Regex and added check for expired Card

--- server-web/src/main/java/it/unimib/sd2024/Common.java
+++ server-web/src/main/java/it/unimib/sd2024/Common.java
@@ -1,5 +1,9 @@
 package it.unimib.sd2024;
 
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Common {
     public static boolean isCardNumberValid(String cardNumber) {
         return cardNumber.matches("\\d{16}");
@@ -10,7 +14,18 @@ public class Common {
     }
 
     public static boolean isCardExpireDateValid(String cardExpireDate) {
-        String regex = "^(0[1-9]|1[0-2])\/([0-9]{2})$";
-        return cardExpireDate.matches(regex);
+        String regex = "^(0[1-9]|1[0-2])/([0-9]{2})$";
+        if (!cardExpireDate.matches(regex)) {
+            return false;
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yy");
+        try {
+            YearMonth cardExpiry = YearMonth.parse(cardExpireDate, formatter);
+            YearMonth currentYearMonth = YearMonth.now();
+            return cardExpiry.isAfter(currentYearMonth) || cardExpiry.equals(currentYearMonth);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
     }
 }
